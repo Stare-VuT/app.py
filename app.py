5f3d86b034c73768bcd4755b9552822f3d83c001
Stare-VuT <nguyenbaoduc9999@gmail.com>
2026-03-28 19:28:14 +0700

    Update app.py
    .

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-cd /storage/emulated/0/Android/data/ru.iiec.pydroid3/files/login-system
-
-cat > app.py << 'EOF'
 from flask import Flask
-
-print(">>> APP STARTING...")
 
 app = Flask(__name__)
 
@@ -12,6 +7,4 @@ def home():
     return "Flask local OK"
 
 if __name__ == "__main__":
-    print(">>> RUNNING FLASK...")
     app.run(host="0.0.0.0", port=5000, debug=True)
-EOF
